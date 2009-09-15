feb2e711b163324f637baf9b2e6ac72868889d3f
Fawzi Mohamed <fawzi@gmx.ch>
2009-09-15 14:53:10 +0200

    fixed rotation v1v2

--- dchem/util/Rotate.d
+++ dchem/util/Rotate.d
@@ -1,16 +1,73 @@
 /// rotations
-module dchem.Rotate;
+module dchem.util.Rotate;
+import blip.narray.NArray;
+import tango.util.log.Trace;
 
-/// rotation from one unit vector v1 to a unit vector v2
-M rotate(M,V)(V v1,V v2,M m){
-    real c,s;
-    c=dot(v1,v2);
-    res=outer(2*c*v2-v1,dot(v2,m))+outer(v1,dot(v2,m));
-    if (c==1.0){
-        s=1.0;
+/// rotation from one *unit* vector v1 to a *unit* vector v2
+M rotateV1V2(V,M)(V v1,V v2,M m){
+    auto c=dot(v1,v2);
+    alias typeof(c) S;
+    auto s2=cast(S)1-c*c;
+    S vOrtC;
+    if (s2>0){
+      vOrtC=(c-cast(S)1)/s2;
     } else {
-        s=sqrt(1.0-c);
+      vOrtC=cast(S)(-1)/cast(S)2;
     }
-    m-=outer(v1,dot(v1/s**2,m))-outer(v2-c*v1,dot(v2-c*v1,m)/s**2);
+    auto v1m=dot(v1,m);
+    auto v2Ortho=v2.dup;
+    v2Ortho-=c*v1;
+    auto v2OrthoM=dot(v2Ortho,m);
+    outer(v2-v1,v1m,m,cast(S)1,cast(S)1);
+    outer(vOrtC*v2Ortho-v1,v2OrthoM,m,cast(S)1,cast(S)1);
+    return m;
 }
+
+/+ to debug
+/// rotation from one delta i vector to a *unit* vector v2
+M rotateEiV1(V,M)(size_t i,V v2,M m){
+    auto c=v2[i];
+    alias typeof(c) S;
+    auto s2=cast(S)1-c*c;
+    S vOrtC;
+    if (s2>0){
+      vOrtC=(c-cast(S)1)/s2;
+    } else {
+      vOrtC=cast(S)(-1)/cast(S)2;
+    }
+    auto v1m=m[i];
+    auto v2Ortho=v2.dup;
+    auto v2OrthoM=dot(v2,m);
+    v2OrthoM[i]=cast(S)0;
+    v2Ortho[i]=v2Ortho[i]-cast(S)1;
+    outer(v2Ortho,v1m,m,cast(S)1,cast(S)1);
+    v2Ortho*=vOrtC;
+    v2Ortho[i]=cast(S)(-1);
+    outer(v2Ortho,v2OrthoM,m,cast(S)1,cast(S)1);
+    return m;
+}
+
+/// rotation from one *unit* vector v2 to the vector delta_i (transpose of the previous one)
+M rotateV1Ei(V,M)(V v2,size_t i,M m){
+    auto c=v2[i];
+    alias typeof(c) S;
+    auto s2=cast(S)1-c*c;
+    S vOrtC;
+    if (s2>0){
+      vOrtC=(c-cast(S)1)/s2;
+    } else {
+      vOrtC=cast(S)(-1)/cast(S)2;
+    }
+    auto v1m=m[i];
+    auto v2Ortho=v2.dup;
+    auto v2OrthoM=dot(v2,m);
+    v2OrthoM[i]=cast(S)0;
+    v2Ortho[i]=v2Ortho[i]-cast(S)1;
+    outer(v1m,v2Ortho,m,cast(S)1,cast(S)1);
+    v2Ortho*=vOrtC;
+    v2Ortho[i]=cast(S)-1;
+    outer(v2OrthoM,v2Ortho,m,cast(S)1,cast(S)1);
+    return m;
+}
++/
 
---
+++ testUtilRotate.d
@@ -0,0 +1,90 @@
+module testUtilRotate;
+import blip.narray.NArray;
+import blip.rtest.RTest;
+import dchem.util.Rotate;
+import tango.math.random.Random;
+import tango.util.log.Trace;
+
+// tests rotation routine v1 v2 in 3D
+void doTestRotV1V2(T)(T v10,T v11,T v12,T v20,T v21,T v22){
+  T[3] v1,v2;
+  v1[]=[v10,v11,v12];
+  v2[]=[v20,v21,v22];
+  auto n1=a2NAof!(T)(v1);
+  n1/=norm2(n1);
+  auto n2=a2NAof!(T)(v2);
+  n2/=norm2(n2);
+
+  auto rotM=rotateV1V2!(NArray!(T,1),NArray!(T,2))(n1,n2,eye!(T)(3));
+  auto err=minFeqrel2(dot(rotM,n1),n2);
+  auto err2=minFeqrel2(dot(rotM,rotM.T),eye!(T)(3));
+  if (err<T.mant_dig/4*3-10){
+    Trace.formatln("err: {}/{}",err,T.mant_dig);
+    throw new Exception("incorrect rotation",__FILE__,__LINE__);
+  }
+  if (err2<T.mant_dig/4*3-10){
+    Trace.formatln("err2: {}/{}",err2,T.mant_dig);
+    throw new Exception("incorrect rotation",__FILE__,__LINE__);
+  }
+}
+/// tests rotation routines v1 j, i v2
+void doTestRotV1J(T)(T v10,T v11,T v12,size_t i){
+  T[3] v1,v2;
+  v1[]=[v10,v11,v12];
+  size_t idx=i%3;
+  auto n1=a2NAof!(T)(v1);
+  n1/=norm2(n1);
+  auto n2=zeros!(T)(3);
+  n2[idx]=cast(T)1;
+
+  auto rotM=rotateV1Ei!(NArray!(T,1),NArray!(T,2))(n1,idx,eye!(T)(3));
+  auto err=minFeqrel2(dot(rotM,n1),n2);
+  auto err2=minFeqrel2(dot(rotM,rotM.T),eye!(T)(3));
+  if (err<T.mant_dig/4*3-10){
+    Trace.formatln("err: {}/{}",err,T.mant_dig);
+    throw new Exception("incorrect rotation",__FILE__,__LINE__);
+  }
+  if (err2<T.mant_dig/4*3-10){
+    Trace.formatln("err2: {}/{}",err2,T.mant_dig);
+    throw new Exception("incorrect rotation",__FILE__,__LINE__);
+  }
+
+  auto rotM_2=rotateEiV1!(NArray!(T,1),NArray!(T,2))(idx,n1,eye!(T)(3));
+  auto err_2=minFeqrel2(dot(rotM_2,n2),n1);
+  auto err2_2=minFeqrel2(dot(rotM_2,rotM_2.T),eye!(T)(3));
+  if (err_2<T.mant_dig/4*3-10){
+    Trace.formatln("err: {}/{}",err_2,T.mant_dig);
+    throw new Exception("incorrect rotation",__FILE__,__LINE__);
+  }
+  if (err2_2<T.mant_dig/4*3-10){
+    Trace.formatln("err2: {}/{}",err2_2,T.mant_dig);
+    throw new Exception("incorrect rotation",__FILE__,__LINE__);
+  }
+  
+  auto err_3=minFeqrel2(rotM.T,rotM_2);
+  if (err_3<T.mant_dig/4*3-10){
+    Trace.formatln("err: {}/{}",err_3,T.mant_dig);
+    throw new Exception("inconsistent V1Ei EiV1",__FILE__,__LINE__);
+  }
+}
+
+void addRotTstToCollection(T)(TestCollection coll){
+  autoInitTst.testNoFail("rotV1V2!("~T.stringof~")",(T v10,T v11,T v12,T v20,T v21,T v22){doTestRotV1V2!(T)(v10,v11,v12,v20,v21,v22);},
+    __LINE__,__FILE__,coll);
+  /+ autoInitTst.testNoFail("rotV1J!("~T.stringof~")",(T v10,T v11,T v12,size_t i){doTestRotV1J!(T)(v10,v11,v12,i);},
+    __LINE__,__FILE__,coll);+/
+}
+
+TestCollection rotateTests(TestCollection superColl){
+    TestCollection coll=new TestCollection("rotV1V2",
+        __LINE__,__FILE__,superColl);
+    addRotTstToCollection!(float)(coll);
+    addRotTstToCollection!(double)(coll);
+    addRotTstToCollection!(real)(coll);
+    return coll;
+}
+
+void main(char[][] args){
+    Stdout(rand.toString()).newline;
+    mainTestFun(args,rotateTests(null));
+}
