79b106505b25d57cca2aa0df054b470db67cbb59
Fawzi Mohamed <fawzi@gmx.ch>
2012-04-14 22:41:06 +0200

    Neighs: toward a real neigh testing

--- dchem/test/NeighTests.d
+++ dchem/test/NeighTests.d
@@ -73,7 +73,7 @@ struct NeighId{
     }
 }
 
-class NeighList(T) {
+class NeighListTester(T) {
     int[NeighId] neighList;
     Cell!(T) cell;
     int expectedValue;
@@ -82,9 +82,13 @@ class NeighList(T) {
     HierarchicalSort!(T) hNeighs;
     PIndex iPart1;
     uint iPos;
+    PIndex p1;
+    KindIdx kind1, kind2;
     RandomSegmentedArray!(T) rArray;
     SafeT cutoff2;
     bool useLock=false;
+    bool weakVerify=false;
+    char[] context;
     
     int addNeigh(ref NeighId nId){
         auto v = nId in neighList;
@@ -111,10 +115,20 @@ class NeighList(T) {
             ret=addNeigh(nId);
         }
         if (ret != expectedValue) {
-            throw new Exception(collectAppender(delegate void(CharSink s){
-                dumper(s)("doLoop: Error for neighs ")(neighs)(" expected ")(expectedValue)
-                    (", had ")(ret);
-            }),__FILE__,__LINE__);
+            if (weakVerify && ret==0) {
+                if (useLock){
+                    synchronized(this){
+                        neighList[nId] += expectedValue-1;
+                    }
+                } else {
+                    neighList[nId] += expectedValue-1;
+                }
+            } else {
+                throw new Exception(collectAppender(delegate void(CharSink s){
+                    dumper(s)("doLoop: Error for neighs ")(neighs)(" expected ")(expectedValue)
+                        (", had ")(ret)(" in ")(context);
+                }),__FILE__,__LINE__);
+            }
         }
         return 0;
     }
@@ -122,66 +136,138 @@ class NeighList(T) {
     void verifyValues(){
         foreach (k,v; neighs){
             if (v!=expectedValue) {
-                throw new Exception(collectAppender(delegate void(CharSink s){
-                    dumper(s)("verifyValues: Error for neighs ")(k)(" expected ")(expectedValue)
-                        (", had ")(v);
-                }),__FILE__,__LINE__);
+                if (weakVerify && v==expectedValue-1) {
+                    neighList.remove[k];
+                } else {
+                    throw new Exception(collectAppender(delegate void(CharSink s){
+                        dumper(s)("verifyValues: Error for neighs ")(k)(" expected ")(expectedValue)
+                            (", had ")(v)(" in ")(context);
+                    }),__FILE__,__LINE__);
+                }
             }
         }
     }
     
-    void setup() {
+    this(RandomCell!(T) rcell, RandomSegmentedArray!(T) rArray) {
+        this.cell=rcell.cell;
+        this.pos=rArray.randomArray();
         auto sGenTest=new SimpleNeighGen();
         sNeighs=sGenTest.createOnConfig(cell,pos);
         auto hGenTest=new HierarchicalSortGen();
         hNeighs=hGenTest.createOnConfig(cell,pos);
     }
     
-    void tests(uint iPart1, uint iPos1, RandomSegmentedArray!(T) rArray, SafeT cutoff2){
-        this.rArray=rArray;
+    void tests(uint iPart1, uint iPos1, uint ikind2, Vector!(T,3) randomR, SafeT cutoff2,
+        SizeLikeNumber!(4,1,-3) bs, bool avoidDoubleCount){
         this.iPart1=iPart1;
-        this.pos=rArray.randomArray();
-        auto particless=this.pos.arrayStruct.submapping().sortedPIndex();
+        this.blockSize=bs.value;
+        auto particles=this.pos.arrayStruct.submapping().sortedPIndex();
         p1=particles[iPos1%particles.length];
-        this.iPos1=iPos1%this.pos.arrayStruct.nParticles(p1.kind);
+        this.iPos1=(iPos1%particles.length)%this.pos.arrayStruct.nParticles(p1.kind);
+        this.avoidDoubleCount = avoidDoubleCount;
+        this.kind1=p1.kind;
+        this.kind2=cast(KindIdx)(particles.kRange.kStart+(ikind2%(particles.kRange.kEnd-particles.kRange.kStart)));
         this.cutoff2=cutoff2;
+        
         useLock=false;
-        sNeighs.sloopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        context="sloopOnNeighsPK_1";
+        sNeighs.sloopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,avoidDoubleCount);
         ++expectedValue;
         verifyValues();
-        sNeighs.sloopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        context="sloopOnNeighsPK_2";
+        sNeighs.sloopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,avoidDoubleCount);
         ++expectedValue;
         verifyValues();
         useLock=true;
-        hNeighs.ploopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        context="ploopOnNeighsPK_1";
+        hNeighs.ploopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,avoidDoubleCount, blockSize);
         ++expectedValue;
         verifyValues();
-        hNeighs.ploopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        context="ploopOnNeighsPK_2";
+        hNeighs.ploopOnNeighsPK(p1, iPos1,kind2,cutoff2, &doLoop,avoidDoubleCount, blockSize);
         ++expectedValue;
         verifyValues();
+
+        int[NeighId] neighListConf;
+        foreach(n,i;neighList){
+            NeighId nConv=n;
+            PIndex dummy;
+            nConv.p1=dummy;
+            nConv.i1=0;
+            neighListConf[nConv]=i;
+        }
         neighList.clear();
-        sNeighs.sloopOnNeighsKK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        neighList=neighListConf;
+
+        useLock=false;
+        weakVerify=true;
+        context="sloopOnNeighsRK_1";
+        sNeighs.sloopOnNeighsRK(r1,kind2,cutoff2, &doLoop,avoidDoubleCount);
         ++expectedValue;
         verifyValues();
-        sNeighs.sloopOnNeighsKK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        weakVerify=false;
+        context="sloopOnNeighsRK_2";
+        sNeighs.sloopOnNeighsRK(r1,kind2,cutoff2, &doLoop,avoidDoubleCount);
         ++expectedValue;
         verifyValues();
-        hNeighs.sloopOnNeighsKK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        useLock=true;
+        context="ploopOnNeighsRK_1";
+        hNeighs.ploopOnNeighsRK(r1,kind2,cutoff2, &doLoop,avoidDoubleCount,blockSize);
         ++expectedValue;
         verifyValues();
-        hNeighs.sloopOnNeighsKK(p1, iPos1,kind2,cutoff2, &doLoop,false);
+        context="ploopOnNeighsRK_2";
+        hNeighs.ploopOnNeighsRK(r1,kind2,cutoff2, &doLoop,avoidDoubleCount,blockSize);
+        ++expectedValue;
+        verifyValues();
+
+        neighList.clear();
+        useLock=false;
+        context="sloopOnNeighsRK_1b";
+        sNeighs.sloopOnNeighsRK(randomR,kind2,cutoff2, &doLoop,avoidDoubleCount);
+        ++expectedValue;
+        verifyValues();
+        context="sloopOnNeighsRK_2b";
+        sNeighs.sloopOnNeighsRK(randomR,kind2,cutoff2, &doLoop,avoidDoubleCount);
+        ++expectedValue;
+        verifyValues();
+        useLock=true;
+        context="ploopOnNeighsRK_1b";
+        hNeighs.ploopOnNeighsRK(randomR,kind2,cutoff2, &doLoop,avoidDoubleCount,blockSize);
+        ++expectedValue;
+        verifyValues();
+        context="ploopOnNeighsRK_2b";
+        hNeighs.ploopOnNeighsRK(randomR,kind2,cutoff2, &doLoop,avoidDoubleCount,blockSize);
+        ++expectedValue;
+        verifyValues();
+
+        neighList.clear();
+        useLock=false;
+        context="sloopOnNeighsKK_1";
+        sNeighs.sloopOnNeighsKK(kind1,kind2,cutoff2, &doLoop,avoidDoubleCount);
+        ++expectedValue;
+        verifyValues();
+        context="sloopOnNeighsKK_2";
+        sNeighs.sloopOnNeighsKK(kind1,kind2,cutoff2, &doLoop,avoidDoubleCount);
+        ++expectedValue;
+        verifyValues();
+        useLock=true;
+        context="ploopOnNeighsKK_1";
+        hNeighs.ploopOnNeighsKK(kind1,kind2,cutoff2, &doLoop,avoidDoubleCount,blockSize);
+        ++expectedValue;
+        verifyValues();
+        context="ploopOnNeighsKK_2";
+        hNeighs.ploopOnNeighsKK(kind1,kind2,cutoff2, &doLoop,avoidDoubleCount,blockSize);
         ++expectedValue;
         verifyValues();
         
-        /// loops on the neighbor of the point r with kind k (p1 will be invalid, i1=0)
-        int sloopOnNeighsRK(Vector!(T,3) r,KindIdx k,SafeT cutoff2,LoopPairs loopBody,
-            size_t optSize=defaultOptimalBlockSize);
-        /// loops on the neighbor of p with kind k
-        int sloopOnNeighsPK(PIndex p1,index_type i1,KindIdx k,SafeT cutoff2,LoopPairs loopBody, bool avoidDoubleCount=false,
-            size_t optSize=defaultOptimalBlockSize);
-        /// loops on the neighbors between particles of kind k1 and k2
-        int sloopOnNeighsKK(KindIdx k1,KindIdx k2,SafeT cutoff2,LoopPairs loopBody, bool avoidDoubleCount=false,
-            size_t optSize=defaultOptimalBlockSize);
-        
+        neighList.clear();
     }
+}
+
+void doNeigListTests(T)(RandomCell!(T) rcell, RandomSegmentedArray!(T) rArray,
+    uint iPart1, uint iPos1, uint ikind2, Vector!(T,3) randomR, SafeT cutoff2,
+    SizeLikeNumber!(4,1,-3) bs, bool avoidDoubleCount)
+{
+    auto tester=new NeighListTester!(T)();
+    //tester.tests()
 }
