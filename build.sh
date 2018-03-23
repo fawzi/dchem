e7d9e09a3826ad0151e4788b8b2afe569accfb36
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2018-03-23 14:33:59 +0100

    adding build commands (two versions)

---
+++ build.sh
@@ -0,0 +1,2 @@
+../xfbuild/xfbuild +x=blip +x=tango +x=std +v +mbm +o=Sampler -I/d/blip -L-L/d/blip/libs -L-lblip-dmd-dbg -L-llapack -L-lblas -L-lhwloc -L-ltango-user-dbg Sampler.d
+../xfbuild/xfbuild +x=tango.core.rt +x=tango.core.vendor +x=std +v +mbm +o=Sampler -I/d/blip -L-L/d/blip/libs -L-lblip-dmd-dbg -L-llapack -L-lblas -L-lhwloc -L-lev -g Sampler.d
